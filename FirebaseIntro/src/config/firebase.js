b2c7d1cb4c28bbbfe1e88bbb60a43d5c6ca11d7b
Vishal Sharma <vishalsharma02731@gmail.com>
2025-08-05 18:13:40 +0530

    remove keys from firebase.js

--- FirebaseIntro/src/config/firebase.js
+++ FirebaseIntro/src/config/firebase.js
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBeRhvvModDFiMoDGbNMWOAuEkif3Tul_w",
-  authDomain: "fir-intro-3eca8.firebaseapp.com",
-  projectId: "fir-intro-3eca8",
-  storageBucket: "fir-intro-3eca8.firebasestorage.app",
-  messagingSenderId: "860089262185",
-  appId: "1:860089262185:web:53760c5ec37526c284c2f8",
-  measurementId: "G-LDLKHE4223"
+  // Your api key
 };
 
 const app = initializeApp(firebaseConfig);
